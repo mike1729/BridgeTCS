813f7447000ab822cb6bd058bc61c7bd855bbaee
pitex <michalpiekarz09@gmail.com>
2014-05-20 15:42:59 +0200

    Printer changed

--- src/ui/text/Trick.cpp
+++ src/ui/text/Trick.cpp
@@ -8,18 +8,7 @@ namespace text
 
 void Trick::print(const bridge::Play::Trick & trick)
 {
-	std::cout << "Current Trick:\n";
-	int currentPlayer = trick.getInitiator();
-	for(auto & card : trick.getCards())
-	{
-		std::cout << "\tPlayer " << currentPlayer + 1 << ":" << Printer::print(card.rank) << " " << Printer::print(card.suit) << "\n";
-		currentPlayer = (currentPlayer + 1)%4;
-	}
-	if(trick.full())
-		std::cout << "Player " << trick.getWinner() + 1 << "won this trick.\n";
-	else
-		std::cout << "Player " << trick.getWinner() + 1 << "leading.\n";
-	std::cout << "\n";
+	Printer::print(trick);
 }
 
 } // namespace text
