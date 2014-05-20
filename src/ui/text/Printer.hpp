63086bf7f6a483441b4781b0eba63b82a2eb56da
pitex <michalpiekarz09@gmail.com>
2014-05-20 17:30:46 +0200

    Printer changes

--- src/ui/text/Printer.hpp
+++ src/ui/text/Printer.hpp
@@ -15,10 +15,51 @@ class Printer
 	static std::string suitSymbols[];
 	static char rankSymbols[];
 public:
-	inline static void print(bridge::Rank const &);
-	inline static void print(bridge::Suit const &);
-	inline static void print(bridge::Hand const &);
-	inline static void print(bridge::Play::Trick const &);
+	inline static void print(bridge::Rank const & rank)
+	{
+		std::cout << rankSymbols[(int) rank];
+	}
+	inline static void print(bridge::Suit const & suit)
+	{
+		std::cout << suitSymbols[(int) suit];
+	}
+
+	inline static void print(bridge::Hand const & hand)
+	{
+		std::cout << "Your cards:\n";
+		
+		for(auto & card : hand.getCards())
+		{
+			Printer::print(card.rank);
+			Printer::print(card.suit);
+			std::cout << "\n";
+		}
+
+		std::cout << std::endl;
+	}
+
+	inline static void print(bridge::Play::Trick const & trick)
+	{
+		std::cout << "Current Trick:\n";
+		int currentPlayer = trick.getInitiator();
+		for(auto & card : trick.getCards())
+		{
+			std::cout << "\tPlayer " << currentPlayer + 1 << ":";
+
+			Printer::print(card.rank);
+			Printer::print(card.suit);
+			
+			std::cout << std::endl;
+			
+			currentPlayer = (currentPlayer + 1)%4;
+		}
+		if(trick.full())
+			std::cout << "Player " << trick.getWinner() + 1 << "won this trick.\n";
+		else
+			std::cout << "Player " << trick.getWinner() + 1 << "leading.\n";
+		
+		std::cout << std::endl;
+	}
 };
 
 } // namespace text
