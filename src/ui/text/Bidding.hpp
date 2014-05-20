44cb5e31b0ec1ab606425249c7620200ba0bccfa
kkll <11zodiak@wp.pl>
2014-05-20 12:45:03 +0200

    UI

---
+++ src/ui/text/Bidding.hpp
@@ -0,0 +1,21 @@
+#ifndef Hand_hpp
+#define Hand_hpp
+
+#include "bridge/Contract.hpp"
+
+namespace ui
+{
+namespace text
+{
+class Play
+{
+public:
+	void notify(const bridge::Bidding & bidding);
+private:
+	
+};
+}
+}
+
+
+#endif
---
+++ src/ui/text/Play.hpp
@@ -0,0 +1,27 @@
+#ifndef UI_PLAY_DEAL_HPP
+#define UI_PLAY_DEAL_HPP
+
+#include "Trick.hpp"
+
+namespace ui
+{
+namespace text
+{
+class Play
+{
+public:
+	void registerTrick(const bridge::Play & play)
+	{
+		play.getTrick().sigModified.connect([& trickView](Trick const & trick) {
+			trickView.print(trick);
+		});
+	}
+private:
+	ui::text::Trick trickView;
+	
+};
+}
+}
+
+
+#endif
