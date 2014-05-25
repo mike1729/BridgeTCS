368794a39dc5e404cd72d136ae8e05927a8acb83
kkll <kkleiner@v-lo.krakow.pl>
2014-05-25 14:24:50 +0200

    UI::Text::Printer refactored

--- src/ui/text/Printer.cpp
+++ src/ui/text/Printer.cpp
@@ -9,52 +9,67 @@ namespace text
 std::string Printer::suitSymbols[] = {"\u2663", "\u2666", "\u2665", "\u2660"};
 char Printer::rankSymbols[] = {'2','3','4','5','6','7','8','9','T','J','Q','K','A'};
 
-// inline void Printer::print(bridge::Rank const & rank)
-// {
-// 	std::cout << rankSymbols[(int) rank];
-// }
 
-// inline void Printer::print(bridge::Suit const & suit)
-// {
-// 	std::cout << suitSymbols[(int) suit];
-// }
-
-// inline void Printer::print(bridge::Hand const & hand)
-// {
-// 	std::cout << "Your cards:\n";
-	
-// 	for(auto & card : hand.getCards())
-// 	{
-// 		Printer::print(card.rank);
-// 		Printer::print(card.suit);
-// 		std::cout << "\n";
-// 	}
-
-// 	std::cout << std::endl;
-// }
-
-// inline void Printer::print(bridge::Play::Trick const & trick)
-// {
-// 	std::cout << "Current Trick:\n";
-// 	int currentPlayer = trick.getInitiator();
-// 	for(auto & card : trick.getCards())
-// 	{
-// 		std::cout << "\tPlayer " << currentPlayer + 1 << ":";
-
-// 		Printer::print(card.rank);
-// 		Printer::print(card.suit);
-		
-// 		std::cout << std::endl;
-		
-// 		currentPlayer = (currentPlayer + 1)%4;
-// 	}
-// 	if(trick.full())
-// 		std::cout << "Player " << trick.getWinner() + 1 << "won this trick.\n";
-// 	else
-// 		std::cout << "Player " << trick.getWinner() + 1 << "leading.\n";
-	
-// 	std::cout << std::endl;
-// }
-
+/*static*/ void Printer::print(bridge::Call const & call)
+{
+	bridge::CallType callType = call.type;
+	if(callType == bridge::CallType::BID)
+	{
+		std::cout << call.level << " ";
+		Printer::print(call.denomination);
+		std::cout << "\n";
+	}
+	else if(callType == bridge::CallType::DOUBLE)
+		std::cout << "Double\n";
+	else if(callType == bridge::CallType::REDOUBLE)
+		std::cout << "Redouble\n";
+	else if(callType == bridge::CallType::PASS)
+		std::cout << "PASS\n";
 }
+
+
+/*static*/ void Printer::print(bridge::Hand const & hand)
+{
+	std::cout << "Your cards:\n";
+	
+	for(auto & card : hand.getCards())
+	{
+		Printer::print(card.rank);
+		Printer::print(card.suit);
+		std::cout << "\n";
+	}
+	std::cout << std::endl;
 }
+
+/*static*/ void Printer::print(bridge::Play::Trick const & trick)
+{
+	std::cout << "Current Trick:\n";
+	int currentPlayer = trick.getInitiator();
+	for(auto & card : trick.getCards())
+	{
+		std::cout << "\tPlayer " << currentPlayer + 1 << ":";
+		Printer::print(card.rank);
+		Printer::print(card.suit);
+		
+		std::cout << std::endl;
+		
+		currentPlayer = (currentPlayer + 1)%4;
+	}
+	if(trick.full())
+		std::cout << "Player " << trick.getWinner() + 1 << "won this trick.\n";
+	else
+		std::cout << "Player " << trick.getWinner() + 1 << "leading.\n";
+		
+	std::cout << std::endl;
+}
+	
+/*static*/ void Printer::print(bridge::Bidding const & bidding)
+{
+	std::cout << "Player " << bidding.getCurrentCaller() << ": ";
+	bridge::Call call = bidding.getLastCall();
+	Printer::print(call); 
+}
+
+
+} //namespace text
+} //namespace ui
--- src/ui/text/Printer.hpp
+++ src/ui/text/Printer.hpp
@@ -16,6 +16,7 @@ class Printer
 {
 	static std::string suitSymbols[];
 	static char rankSymbols[];
+
 public:
 	inline static void print(bridge::Rank const & rank)
 	{
@@ -32,67 +33,10 @@ public:
 		else std::cout << suitSymbols[nr];
 	}
 
-	inline static void print(bridge::Call const & call)
-	{
-		bridge::CallType callType = call.type;
-		if(callType == bridge::CallType::BID)
-		{
-			std::cout << call.level << " ";
-			Printer::print(call.denomination);
-			std::cout << "\n";
-		}
-		else if(callType == bridge::CallType::DOUBLE)
-			std::cout << "Double\n";
-		else if(callType == bridge::CallType::REDOUBLE)
-			std::cout << "Redouble\n";
-		else if(callType == bridge::CallType::PASS)
-			std::cout << "PASS\n";
-	}
-
-
-	inline static void print(bridge::Hand const & hand)
-	{
-		std::cout << "Your cards:\n";
-		
-		for(auto & card : hand.getCards())
-		{
-			Printer::print(card.rank);
-			Printer::print(card.suit);
-			std::cout << "\n";
-		}
-
-		std::cout << std::endl;
-	}
-
-	inline static void print(bridge::Play::Trick const & trick)
-	{
-		std::cout << "Current Trick:\n";
-		int currentPlayer = trick.getInitiator();
-		for(auto & card : trick.getCards())
-		{
-			std::cout << "\tPlayer " << currentPlayer + 1 << ":";
-
-			Printer::print(card.rank);
-			Printer::print(card.suit);
-			
-			std::cout << std::endl;
-			
-			currentPlayer = (currentPlayer + 1)%4;
-		}
-		if(trick.full())
-			std::cout << "Player " << trick.getWinner() + 1 << "won this trick.\n";
-		else
-			std::cout << "Player " << trick.getWinner() + 1 << "leading.\n";
-		
-		std::cout << std::endl;
-	}
-	
-	inline static void print(bridge::Bidding const & bidding)
-	{
-		std::cout << "Player " << bidding.getCurrentCaller() << ": ";
-		bridge::Call call = bidding.getLastCall();
-		Printer::print(call); 
-	}
+	static void print(bridge::Call const & call);
+	static void print(bridge::Hand const & hand);
+	static void print(bridge::Play::Trick const & trick);
+	static void print(bridge::Bidding const & bidding);
 };
 
 } // namespace text
