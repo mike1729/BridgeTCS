1f9f716c14a7d12aa20ac80b9a332317b9195035
laakhbeenor <helena@ubuntu.(none)>
2014-05-26 23:43:19 +0200

    App changes more

--- src/viewmodel/Application.hpp
+++ src/viewmodel/Application.hpp
@@ -4,6 +4,7 @@
 #include "ui/Observable.hpp"
 #include "bridge/Player.hpp"
 #include "ui/text/PlayerUI.hpp"
+#include "ui/text/Game.hpp"
 #include "bridge/Game.hpp"
 #include "Bot.hpp"
 
@@ -16,11 +17,15 @@ public:
 	{	
 		std::array<std::unique_ptr<bridge::IPlayer>,4> players{ { std::unique_ptr<bridge::IPlayer>(new ui::text::PlayerUI()),std::unique_ptr<bridge::IPlayer>(new Bot()),std::unique_ptr<bridge::IPlayer>(new Bot()),std::unique_ptr<bridge::IPlayer>(new Bot()) } };
 		game = new bridge::Game(players);
-		game->start();
+		game->sigModified.connect([this](bridge::Game const & game) {
+					this->gameView.registerDeal(game);
+				});
 		sigModified(*this);
+		game->start();
 	}
 private:
 	bridge::Game* game; 
+	ui::text::Game gameView;
 };
 
 }
