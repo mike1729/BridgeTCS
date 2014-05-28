cb3ca437bab9cc462cfa915a3f699225951a8b5f
timorl <tymorl@gmail.com>
2014-05-28 16:32:32 +0200

    HandTest refactored

--- src/bridge/test/HandTest.cpp
+++ src/bridge/test/HandTest.cpp
@@ -7,40 +7,37 @@ namespace bridge {
 class HandTest: public ::testing::Test
 {
 	public:
-		Standard52Deck *deck;
-		Hand *hand;
+		Hand hand;
+		Card c1 = Card(Rank::TWO, Suit::CLUBS);
+		Card c2 = Card(Rank::TWO, Suit::DIAMONDS);
+		Card c3 = Card(Rank::TWO, Suit::HEARTS);
+		Card c4 = Card(Rank::TWO, Suit::SPADES);
 		void SetUp()
 		{
-			deck = new Standard52Deck();
-			hand = new Hand();
+			hand.removeAll();
 		}
 		void TearDown()
 		{
-			delete deck;
-			delete hand;
 		}
 };
 
 TEST_F(HandTest, SimpleInsertRemove) 
 {
-	Card c = deck->dealCard();
-	Suit s = c.suit;
-	Rank r = c.rank;
-	hand->insert(c);
-	Card c1 = hand->remove(0);
-	EXPECT_EQ(c1.suit, s);
-	EXPECT_EQ(c1.rank, r);
+	hand.insert(c1);
+	Card c = hand.remove(0);
+	EXPECT_EQ(c1.suit, c.suit);
+	EXPECT_EQ(c1.rank, c.rank);
 }
 
 
 TEST_F(HandTest, SimpleInsertRemoveAll)
 {
-	hand->insert(deck->dealCard());
-	hand->insert(deck->dealCard());
-	hand->insert(deck->dealCard());
-	hand->insert(deck->dealCard());
-	hand->removeAll();
-	EXPECT_EQ(0, hand->getHand().size());
+	hand.insert(c1);
+	hand.insert(c2);
+	hand.insert(c3);
+	hand.insert(c4);
+	hand.removeAll();
+	EXPECT_EQ(0, hand.getCards().size());
 }
 
 }
