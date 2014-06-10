47fdc6473c28f745aeb142f951529ab5aaffe52c
patryku <patryk.u93@gmail.com>
2014-06-10 22:23:27 +0200

    Bug fix: counting extra tricks properly

--- src/ui/text/Deal.cpp
+++ src/ui/text/Deal.cpp
@@ -65,7 +65,7 @@ void Deal::notify(const bridge::Deal & deal)
 
 		case bridge::DealEvent::PlayEnd:
 			bridge::DealResult result = deal.getResult();
-			int extra = result.declarerTakenTricks >= result.contract.level + 6;
+			int extra = result.declarerTakenTricks - result.contract.level - 6;
 			if(extra >= 0)
 				std::cout << "Contract accomplished. " <<  extra << " overtricks.\n";
 			else
