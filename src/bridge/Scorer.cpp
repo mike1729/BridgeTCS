40417179532c3ca690d667b7ae9aadfcb3e4d032
patryku <patryk.u93@gmail.com>
2014-06-17 12:57:45 +0200

    Redundant brackets removed

--- src/bridge/Scorer.cpp
+++ src/bridge/Scorer.cpp
@@ -16,48 +16,36 @@ void Scorer::update(const DealResult & result)
 			contractPoints += 40;
 			contractPoints += 30 * (result.contract.level - 1);
 		} else if ( result.contract.denomination == Denomination::SPADES || result.contract.denomination == Denomination:: HEARTS )
-		{
 			contractPoints += 30 * result.contract.level;
-		} else
-		{
+		else
 			contractPoints += 20 * result.contract.level;
-		}
 		contractPoints *= result.contract.pointMultiplier;
 		declaringTeam.updatePointsBelow(contractPoints);
 		int bonusPoints = 0;
 		if ( result.contract.pointMultiplier == 1 ) // not doubled
 		{
 			if ( result.contract.denomination == Denomination::NT || result.contract.denomination == Denomination::SPADES || result.contract.denomination == Denomination:: HEARTS ) // major suits
-			{
 				bonusPoints += 30*overtricks;
-			} else // minor suits
-			{
+			else // minor suits
 				bonusPoints += 20*overtricks;
-			}
 		} else //doubled or redoubled
 		{
 			bonusPoints += 50*result.contract.pointMultiplier * overtricks;
 			if ( declaringTeam.isVulnerable() ) 
-			{
 				bonusPoints *= 2;
-			}
 			bonusPoints += 25*result.contract.pointMultiplier; // bonus for making (re)doubled
 		}
 		if ( result.contract.level == 6 )
 		{
 			bonusPoints += 500;
 			if ( declaringTeam.isVulnerable() )
-			{
 				bonusPoints += 250;
-			}
 		}
 		if ( result.contract.level == 7 )
 		{
 			bonusPoints += 1000;
 			if ( declaringTeam.isVulnerable() )
-			{
 				bonusPoints += 500;
-			}
 		}
 		if ( contractPoints >= 100 ) //becoming vulnerable, possibly finished rubber
 		{
@@ -65,9 +53,7 @@ void Scorer::update(const DealResult & result)
 			{
 				bonusPoints += 500;
 				if ( !defendingTeam.isVulnerable() )
-				{
 					bonusPoints += 200;
-				}
 			}
 			declaringTeam.setVulnerability(true);
 		}
@@ -79,32 +65,24 @@ void Scorer::update(const DealResult & result)
 		if ( declaringTeam.isVulnerable() )
 		{
 			if ( result.contract.pointMultiplier == 1 ) // not doubled
-			{
 				bonusPoints += 100 * undertricks;
-			} else //doubled or redoubled
+			else //doubled or redoubled
 			{
 				bonusPoints += 100 * result.contract.pointMultiplier * undertricks;
 				if ( undertricks > 1 )
-				{
 					bonusPoints += 50 * result.contract.pointMultiplier * ( undertricks - 1 );
-				}
 			}
 		} else // declaring team not vulnerable
 		{
 			if ( result.contract.pointMultiplier == 1 ) // not doubled
-			{
 				bonusPoints += 50 * undertricks;
-			} else //doubled or redoubled
+			else //doubled or redoubled
 			{
 				bonusPoints += 50 * result.contract.pointMultiplier * undertricks;
 				if ( undertricks > 1 )
-				{
 					bonusPoints += 50 * result.contract.pointMultiplier * ( undertricks - 1 );
-				}
 				if ( undertricks > 3 )
-				{
 					bonusPoints += 50 * result.contract.pointMultiplier * ( undertricks - 3 );
-				}
 			}
 		}
 		defendingTeam.updatePointsAbove(bonusPoints);
