8a67f3c05d58b156255ead440f26dc8505acbf98
loocash <lukaszkapica@gmail.com>
2014-06-26 18:56:30 +0200

    Just refactorization of Printer::print(bridge::Hand const&)
    	modified:   src/ui/text/Printer.cpp

--- src/ui/text/Printer.cpp
+++ src/ui/text/Printer.cpp
@@ -35,94 +35,37 @@ char Printer::rankSymbols[] = {'2','3','4','5','6','7','8','9','T','J','Q','K','
 
 /*static*/ void Printer::print(bridge::Hand const & hand)
 {
-	std::vector<bridge::Card> spades, hearts, diamonds, clubs;
+	bridge::Suit suits[] = {
+		bridge::Suit::SPADES,
+		bridge::Suit::HEARTS,
+		bridge::Suit::DIAMONDS,
+		bridge::Suit::CLUBS
+	};
 
-	for (auto & card : hand.getCards())
+	for (auto & suit : suits)
 	{
-		switch(card.suit)
+		if (suit == bridge::Suit::HEARTS or 
+			suit == bridge::Suit::DIAMONDS)
+	    	std::cout<< "\e[1;31m"; // red
+	    else
+			std::cout<<"\e[1;30m"; // black 
+		Printer::print(suit);
+		std::cout << ": ";
+		auto cardsIt = hand.getCards().rbegin();
+		auto endIt = hand.getCards().rend();
+		while (cardsIt != endIt)
 		{
-			case bridge::Suit::SPADES:
-				spades.push_back(card);
-				break;
-			case bridge::Suit::HEARTS:
-				hearts.push_back(card);
-				break;
-			case bridge::Suit::DIAMONDS:
-				diamonds.push_back(card);
-				break;
-			case bridge::Suit::CLUBS:
-				clubs.push_back(card);
-				break;
-			default:
-				std::cout << "An error occured in Printer::print(bridge::Hand const&)\n";
-				break;
+			if (cardsIt->suit == suit)
+			{
+				Printer::print(cardsIt->rank);
+				std::cout << " ";
+			}
+			cardsIt++;
 		}
-	}
-
-	// set black color for spades
-	std::cout<<"\e[1;30m"; // black
-	std::cout << "S: ";
-
-	for (auto& card : spades)
-	{
-		Printer::print(card.rank);
-		std::cout << " ";
-	}
-	std::cout << "\n";
-
-	// set red color for hearts and diamonds
-	std::cout<< "\e[1;31m"; // red
-	std::cout << "H: ";
-	
-	for (auto& card : hearts)
-	{
-		Printer::print(card.rank);
-		std::cout << " ";
-	}
-	std::cout << "\n";
-
-	std::cout << "D: ";
-	for (auto& card : diamonds)
-	{
-		Printer::print(card.rank);
-		std::cout << " ";
-	}
-	std::cout << "\n";
-
-	// set black color for clubs
-	std::cout<<"\e[1;30m"; // black
-	std::cout << "C: ";
-
-	for (auto& card : clubs)
-	{
-		Printer::print(card.rank);
-		std::cout << " ";
+		std::cout << "\n";
 	}
 	std::cout<<"\e[0m";
-	std::cout << "\n\n";
-/*
-	auto cardsIt = hand.getCards().rbegin();
-	auto endIt = hand.getCards().rend();
-	while (cardsIt != endIt)
-	{
-		if (cardsIt->suit == bridge::Suit::HEARTS or 
-			cardsIt->suit == bridge::Suit::DIAMONDS)
-        	std::cout<< "\e[1;31m"; // red
-    	else
-        	std::cout<<"\e[1;30m"; // black 
-		Printer::print(cardsIt->suit);
-		std::cout << ": ";
-		for (bridge::Suit currentSuit = cardsIt->suit; 
-			currentSuit == cardsIt->suit && cardsIt != endIt; cardsIt++)
-		{
-			Printer::print(cardsIt->rank);
-			std::cout << " ";
-		}
-		std::cout << std::endl;
-		std::cout<<"\e[0m";
-	}
-	std::cout << std::endl;
-*/
+	std::cout << "\n";
 }
 
 /*static*/ void Printer::print(bridge::Play::Trick const & trick)
